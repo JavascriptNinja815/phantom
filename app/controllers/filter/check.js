ca532aaec37cac03ac64d04bdc911d3042610730
javascriptninja815 <xandokan815@protonmail.com>
2019-07-21 20:34:03 +0300

    facebook issue fixed

--- app/controllers/filter/check.js
+++ app/controllers/filter/check.js
@@ -39,14 +39,12 @@ module.exports.checkFilter = (req, link, ip, connection, isOfferPage, usePage, c
 
   if (!isOfferPage) {
     let referer = req.get("referer");
-    console.log('referer', referer);
-    console.log('network', link.network);
     if (link.network === 'Outbrain') {
-      if (!referer && !referer.toLowerCase().includes('paid.outbrain.com'))
+      if (!referer || !referer.toLowerCase().includes('paid.outbrain.com'))
         return cb(false, trafficRecord);
     }
     if (link.network === 'Facebook') {
-      if (!referer && !referer.toLowerCase().includes('m.facebook.com')
+      if (!referer || !referer.toLowerCase().includes('m.facebook.com')
       && !referer.toLowerCase().includes('facebook.com')
       && !referer.toLowerCase().includes('l.facebook.com')
       && !referer.toLowerCase().includes('lm.facebook.com')
